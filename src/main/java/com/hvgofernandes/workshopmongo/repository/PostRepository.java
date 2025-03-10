31b11c67bdd48a6b338f3a953663aaec1f30fbdc
hvgofernandes <hvgofernandes@gmail.com>
2025-03-10 20:22:13 -0300

    feat: @Query alternative

--- src/main/java/com/hvgofernandes/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/hvgofernandes/workshopmongo/repository/PostRepository.java
@@ -2,10 +2,14 @@ package com.hvgofernandes.workshopmongo.repository;
 
 import com.hvgofernandes.workshopmongo.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import java.util.List;
 
 public interface PostRepository extends MongoRepository <Post, String> {
+
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> searchByTitle(String text);
 
     List<Post> findByTitleContainingIgnoreCase(String text);
 }
