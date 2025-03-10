5943f2eb02ba4e5cb61ca4e77b0201752604708d
hvgofernandes <hvgofernandes@gmail.com>
2025-03-10 19:55:03 -0300

    feat: Query methods

--- src/main/java/com/hvgofernandes/workshopmongo/resource/PostResources.java
+++ src/main/java/com/hvgofernandes/workshopmongo/resource/PostResources.java
@@ -2,13 +2,13 @@ package com.hvgofernandes.workshopmongo.resource;
 
 
 import com.hvgofernandes.workshopmongo.domain.Post;
+import com.hvgofernandes.workshopmongo.resource.util.URL;
 import com.hvgofernandes.workshopmongo.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/posts")
@@ -22,4 +22,12 @@ public class PostResources {
         Post obj = postService.findById(id);
         return ResponseEntity.ok().body(obj);
     }
+
+    @RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> findByTitle (@RequestParam(value = "text", defaultValue = "") String text) {
+        text = URL.decodeParam(text);
+        List<Post> list = postService.findByTitle(text);
+        return ResponseEntity.ok().body(list);
+    }
+
 }
